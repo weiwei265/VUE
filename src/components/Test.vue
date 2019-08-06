f7efbb6c5e1ba941b442788670ff8a1a4599324b
weiwei265 <shengwei265@163.com>
2019-08-06 18:39:12 +0800

    first

---
+++ src/components/Test.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id = "didi-navigator">
+		<ul>
+			<li v-for="tab in tabs">
+				{{tab.text}}
+			</li>
+		</ul>
+	</div>
+</template>
+<script>
+export default {
+name:'HelloWorld',
+data(){
+	return {
+   tabs:[
+        {text: 'hello world1'},
+		{text: 'hello world2'},
+		{text: 'hello world3'},
+		{text:'hello world4'},
+		{text:'hello world5'},
+		{text:'hello world6'}
+		]
+  }
+}
+}
+</script>
